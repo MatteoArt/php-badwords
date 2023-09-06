496775a036679da3d091a3e5900bc4e0363b5435
MatteoArt <mattrim8@gmail.com>
2023-09-06 19:15:03 +0200

    implementazione form e raccolta dati

---
+++ badwords.php
@@ -0,0 +1,20 @@
+<?php 
+
+// var_dump($_POST);
+
+$paragrafo = $_POST["par"];
+$parola = $_POST["word"];
+
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Badwords Php</title>
+</head>
+<body>
+    <
+</body>
+</html>
--- index.php
+++ index.php
@@ -4,13 +4,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP Badwords</title>
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
+    <h1>PHP Badwords</h1>
     <form action="badwords.php" method="post">
         <label for="paragraf">Inserisci paragrafo:</label>
         <textarea name="par" id="paragraf" cols="30" rows="10"></textarea>
         <label for="parola">Inserisci parola da censurare:</label>
-        <input type="text" name="word" id="parola">
+        <input type="text" name="word" id="parola"><br>
         <button type="submit">Invia</button>
     </form>
 </body>
